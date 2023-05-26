f07be6f9e6b03edd61e8d8d16c9ce65443d5d65f
Douglas Lima <douglimaonline@gmail.com>
2023-05-26 18:26:52 -0300

    first commit

---
+++ Fundamentos/VariaveisEConstantes.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace csharp.Fundamentos
+{
+    static void Executar() {
+        double raio = 4.5;
+        const double PI = 3.14;
+
+        double area = PI * raio * raio;
+    }
+}
