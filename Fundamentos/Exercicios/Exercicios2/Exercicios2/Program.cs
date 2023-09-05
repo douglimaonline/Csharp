82d53f2a590de92b3205819df04f57acbd275da4
Douglas <douglimaonline@gmail.com>
2023-09-05 16:09:26 -0300

    Csharp Exercices update

--- Fundamentos/Exercicios/Exercicios2/Exercicios2/Program.cs
+++ Fundamentos/Exercicios/Exercicios2/Exercicios2/Program.cs
@@ -1,40 +1,50 @@
 ﻿using System;
 using System.Threading.Channels;
 
-namespace Exercicios {
-    internal class Program {
-        static void Main(string[] args) {
+// --------------------------- Exercícios 2 ---------------------------
 
-            // --------------------------- Exercícios 2 ---------------------------
+Console.WriteLine("--------------------------- Exercício 03 ---------------------------");
 
-            while (true) {
+Console.Write("Informe o primeiro número: ");
+int num1 = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("--------------------------- Exercício 03 ---------------------------");
+Console.Write("Informe o segundo número: ");
+int num2 = int.Parse(Console.ReadLine());
 
-                Console.Write("Informe o primeiro número: ");
-                int num1 = int.Parse(Console.ReadLine());
+if (num1 > num2) {
+    if (num1 % num2 == 0) {
+        Console.WriteLine("São Multiplos.");
+    }
+    else {
+        Console.WriteLine("Não são Multiplos");
+    }
+}
+else {
+    if (num2 % num1 == 0) {
+        Console.WriteLine("São Multiplos.");
+    }
+    else {
+        Console.WriteLine("Não são Multiplos");
+    }
+}
 
-                Console.Write("Informe o segundo número: ");
-                int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("--------------------------- Exercício 08 ---------------------------");
 
-                if (num1 > num2) {
-                    if (num1 % num2 == 0) {
-                        Console.WriteLine("São Multiplos.");
-                    }
-                    else {
-                        Console.WriteLine("Não são Multiplos");
-                    }
-                }
-                else {
-                    if (num2 % num1 == 0) {
-                        Console.WriteLine("São Multiplos.");
-                    }
-                    else {
-                        Console.WriteLine("Não são Multiplos");
-                    }
-                }
+while (true) {
 
-            }
-        }
+    Console.Write("Informe seu salário: ");
+    double salario = double.Parse(Console.ReadLine());
+
+    if (salario <= 2000) {
+        Console.WriteLine("Você está Isento do Imposto de Renda.");
+    }
+    else if ((salario - 2000) <= 3000) {
+        Console.WriteLine($"Você de deve R${((salario - 2000) * 0.08).ToString("F2")} de Imposto de Renda.");
+    }
+    else if ((salario - 2000) <= 4500) {
+        Console.WriteLine($"Você de deve R${((salario - 2000) * 0.18).ToString("F2")} de Imposto de Renda.");
+    }
+    else {
+        Console.WriteLine($"Você de deve R${((salario - 2000) * 0.28).ToString("F2")} de Imposto de Renda.");
     }
 }
