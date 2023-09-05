1730e71c8dfd220adcec0489bc527b02482995bd
Douglas <douglimaonline@gmail.com>
2023-09-05 17:41:48 -0300

    Csharp exercises update

---
+++ Fundamentos/Exercicios/Exercicios4/Exercicios4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercicios {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // --------------------------- Exercícios 4 ---------------------------
+
+            Console.WriteLine("--------------------------- Exercício 06 ---------------------------");
+
+            Console.Write("Informe um numero inteiro: ");
+            int T = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= T; i++) {
+                if (T % i == 0) {
+                    Console.WriteLine(i);
+                }
+            }
+
+           
+
+            Console.WriteLine("--------------------------- Exercício 07 ---------------------------");
+
+            Console.Write("Informe um numero inteiro: ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++) {
+                Console.WriteLine($"{i} {Math.Pow(i, 2)} {Math.Pow(i, 3)}");
+            }
+
+        }
+
+    }
+}
