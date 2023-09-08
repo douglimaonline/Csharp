fcdcb8f06e9b5b7b4307b77f762672d5003acaf6
Douglas <douglimaonline@gmail.com>
2023-09-08 17:31:05 -0300

    Exercicios De Fixacao Membros Staticos

---
+++ Classes Atributos e Metodos/ExerciciosDeFixacao/ExerciciosDeFixacaoMembrosStaticos/ExerciciosDeFixacaoMembrosStaticos/ConversorDeMoeda.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExerciciosDeFixacaoMembrosStaticos {
+    internal class ConversorDeMoeda {
+
+        public static double Calculo(double cotacao, double dolares) {
+            double resultadoBruto = (cotacao * dolares);
+            double resultado = resultadoBruto + (resultadoBruto * 0.06);
+            return resultado;
+        }
+    }
+}
---
+++ Classes Atributos e Metodos/ExerciciosDeFixacao/ExerciciosDeFixacaoMembrosStaticos/ExerciciosDeFixacaoMembrosStaticos/Program.cs
@@ -0,0 +1,21 @@
+﻿using ExerciciosDeFixacaoMembrosStaticos;
+
+namespace Exercicios {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("-------------------- Exercício 1 --------------------\n");
+
+            Console.Write("Qual a cotação em dólar? ");
+            double cotacao = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantos Dolares você vai comprar? ");
+            double dolares = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(ConversorDeMoeda.Calculo(cotacao, dolares).ToString("F2"));
+            
+        }
+
+        
+    }
+}
