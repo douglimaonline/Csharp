9c5e3a1277dc5d973b797074f539b11ff0ed1434
Douglas <douglimaonline@gmail.com>
2023-09-10 23:50:11 -0300

    Febonacci Exercise

---
+++ Fundamentos/Exercicios/Febonacci/Febonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Febonacci {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Informe um numero inteiro: ");
+            int n = int.Parse(Console.ReadLine());
+            List<int> febonacciList = new List<int>();
+            febonacciList.Add(0);
+            febonacciList.Add(1);
+
+            for (int i = 2; i < n; i++) {
+                febonacciList.Add(febonacciList[(febonacciList.Count-2)] + febonacciList[(febonacciList.Count- 1)]);
+            }
+            Console.WriteLine($"Modelo Febonacci com {n} itens.");
+            foreach (var item in febonacciList) {
+                Console.WriteLine(item);
+            }
+            {
+                
+            }
+        } 
+    }
+}
