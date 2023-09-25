b0fb4172cb84ea271db73802307cfcd5d444f7a7
Douglas <douglimaonline@gmail.com>
2023-09-25 16:22:43 -0300

    adding params

---
+++ Classes Atributos e Metodos/Params/Params/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Params {
+    internal class Program {
+        static void Main(string[] args) {
+            double[] numeros = { 1, 2, 3, 4 };
+
+            Console.WriteLine(Calculator.Sum(1, 2, 3, 4, 5));
+        }
+    }
+}
---
+++ Classes Atributos e Metodos/Params/Params/calculator.cs
@@ -0,0 +1,11 @@
+﻿namespace Params {
+    public class Calculator {
+        public static double Sum(params double[] list) {
+            double total = 0;
+            for (int i = 0; i < list.Length; i++) {
+                total += list[i];
+            }
+            return total;
+        }
+    }
+}
