376a72886315e6fd59e1e44ba47d38e8c3ee1785
Douglas <douglimaonline@gmail.com>
2023-09-27 15:38:37 -0300

    Adding Listas

--- Classes Atributos e Metodos/Params/Params/Program.cs
+++ Classes Atributos e Metodos/Params/Params/Program.cs
@@ -1,8 +1,6 @@
 ﻿namespace Params {
     internal class Program {
         static void Main(string[] args) {
-            double[] numeros = { 1, 2, 3, 4 };
-
             Console.WriteLine(Calculator.Sum(1, 2, 3, 4, 5));
         }
     }
---
+++ Memoria Array e Lista/Listas/Listas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Listas {
+internal class Program {
+        static void Main(string[] args) {
+
+            List<string> MyList = new List<string>();
+
+            MyList.Add("Juca");
+            MyList.Add("Doug");
+            MyList.Add("Romuro");
+            MyList.Add("Gili");
+            MyList.Insert(1, "Nina");
+
+
+            foreach (var item in MyList) {
+                Console.WriteLine(item);
+            }
+
+            List<string> NewList = (MyList.FindAll(x => x.Length > 4));
+            Console.WriteLine();
+            foreach (var item in NewList) { Console.WriteLine(item); }
+
+            MyList.RemoveAll(x => x.Length > 4);
+            Console.WriteLine();
+            foreach (var item in MyList) { Console.WriteLine(item); }
+
+        }    
+   }
+}
