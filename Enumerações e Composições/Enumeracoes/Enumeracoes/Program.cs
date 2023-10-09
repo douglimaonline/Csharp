82dda9cf0d54111522369e1280915d9659309929
Douglas <douglimaonline@gmail.com>
2023-10-09 17:13:13 -0300

    Adding enumeracoes e composicoes

---
+++ Enumerações e Composições/Enumeracoes/Enumeracoes/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Enumeracoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> MyList = new List<string> {"doug", "Juca", "Igo", "Thais", "Carol"};
+
+            Console.Write("MyList = ");
+            foreach (var item in MyList)
+            {
+                Console.Write($"{item}, ");
+            }
+        }
+    }
+}
