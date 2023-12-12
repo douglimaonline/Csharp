971a58c1b4122c807394c17f9ccf9acef74ed4d6
douglas <douglimaonline@gmail.com>
2023-12-11 22:25:24 -0300

    Lambdas, Delegates e LINQ

---
+++ Lambdas, delegates, LINQ/LambdasDelegatesLINQ/LambdasDelegatesLINQ/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LambdasDelegatesLINQ
+{
+
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Price}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> Products = new List<Product>
+            {
+                new Product { Name = "TV", Price = 400.00 },
+                new Product { Name = "Table", Price = 150.00 },
+                new Product { Name = "Windown", Price = 160.00 },
+                new Product { Name = "Computer", Price = 340.00 },
+                new Product { Name = "Smathfone", Price = 560.00 }
+            };
+
+            List<Product> Spansive = Products.FindAll(p => p.Price > 200).ToList();
+
+            foreach (Product p in Spansive)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
